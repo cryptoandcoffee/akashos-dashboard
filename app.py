d9a376e5ce00c3ac72fa6ae32345b56cc1809b4c
88plug <19512127+88plug@users.noreply.github.com>
2023-07-04 11:39:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -165,11 +165,11 @@ def dashboard():
         updated_script_lines = []
         for line in script_lines:
             if line.startswith('TARGET_MEMORY='):
-                line = f'TARGET_MEMORY="{existing_variables.get("MEMORY_PRICE", "0.10")}"\n'
+                line = f'TARGET_MEMORY="{existing_variables.get("MEMORY_PRICE", "0.75")}"\n'
             elif line.startswith('TARGET_HD='):
-                line = f'TARGET_HD="{existing_variables.get("DISK_PRICE", "0.10")}"\n'
+                line = f'TARGET_HD="{existing_variables.get("DISK_PRICE", "0.25")}"\n'
             elif line.startswith('TARGET_CPU='):
-                line = f'TARGET_CPU="{existing_variables.get("CPU_PRICE", "2.78")}"\n'
+                line = f'TARGET_CPU="{existing_variables.get("CPU_PRICE", "1.50")}"\n'
             updated_script_lines.append(line)
 
         with open(bid_engine_script_path, 'w') as f:
